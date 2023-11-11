e2f2d9fa6fb6a0b9e19f69a30ecbab64b7edaf37
Mike-Vayner <mv9335@pb.amalnet.k12.il>
2023-11-11 15:37:34 +0200

    Added support for passing argments.

--- src/main.py
+++ src/main.py
@@ -1,33 +1,45 @@
+#!/usr/bin/env python
+
+import argparse
 import hashlib
-import time
 
 
-def increment_string(string: str) -> str:
+class _Args:
+    start: str
+    end: str
+    digest: str
+
+
+def _increment_string(string: str) -> str:
     # if the last character is z it needs to not overflow
     if string[-1] == "z":
         try:
-            return increment_string(string[:-1]) + "a"
+            return _increment_string(string[:-1]) + "a"
         # if the string is empty the length needs to be increased
         except IndexError:
             return (len(string) + 1) * "a"
     return string[:-1] + chr(ord(string[-1]) + 1)
 
 
-def decrypt(start: str, end: str, digest: bytes) -> str:
+def decrypt(start: str, end: str, digest: str) -> str | None:
     if len(end) < len(start):
         raise ValueError
     while start != end:
-        if hashlib.md5(start.encode()).digest() == digest:
+        if hashlib.md5(start.encode()).hexdigest() == digest:
             return start
-        start = increment_string(start)
-    return ""
+        start = _increment_string(start)
+    return None
 
 
 def main():
-    start = time.perf_counter()
-    print(decrypt("aaaaaaaa", "zzzzzzzz", hashlib.md5(b"abcdefgh").digest()))
-    end = time.perf_counter()
-    print(end - start)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("start")
+    parser.add_argument("end")
+    parser.add_argument("digest")
+    args = parser.parse_args(namespace=_Args)
+
+    password = decrypt(args.start, args.end, args.digest)
+    print(password or "Password not found")
 
 
 if __name__ == "__main__":
