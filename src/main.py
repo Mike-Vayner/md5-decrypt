8d7e564ebd656399a2776c6bd58be14db26744f2
Mike-Vayner <mv9335@pb.amalnet.k12.il>
2023-11-16 18:57:41 +0200

    Added the code to connect and chnaged file names.

--- src/main.py
+++ src/main.py
@@ -1,61 +1,43 @@
-#!/usr/bin/env python
+import asyncio
 
-import argparse
-import functools
-import hashlib
-from collections.abc import Iterable
-from concurrent import futures
+from helpers import decrypt
 
 
-class _Args:
-    start: str
-    end: str
-    digest: str
-
-
-def _increment_string(string: str) -> str:
-    for i in range(-1, -len(string) - 1, -1):
-        if string[i] != "z":
-            return f"{string[:i]}{chr(ord(string[i]) + 1)}{'a' * -(i + 1)}"
-    return "a" * (len(string) + 1)
-
-
-def _iterate_strings(start: str, end: str) -> Iterable[str]:
-    yield start
-    while start != end:
-        start = _increment_string(start)
-        yield start
-
-
-def _check_password(password: str, digest: str) -> str | None:
-    if hashlib.md5(password.encode()).hexdigest() == digest:
-        return password
-    return None
-
-
-def decrypt(start: str, end: str, digest: str) -> str | None:
-    if len(end) < len(start):
-        raise ValueError
-    with futures.ProcessPoolExecutor() as executor:
-        check = functools.partial(_check_password, digest=digest)
-        results = executor.map(check, _iterate_strings(start, end), chunksize=128)
-        for res in results:
-            if res is not None:
-                executor.shutdown(False, cancel_futures=True)
-                return res
-    return None
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("start")
-    parser.add_argument("end")
-    parser.add_argument("digest")
-    args = parser.parse_args(namespace=_Args)
-
-    password = decrypt(args.start, args.end, args.digest)
-    print(password or "Password not found")
+async def main():
+    reader, writer = await asyncio.open_connection()
+    writer.write(b"amogus")
+    await writer.drain()
+    my_id = await reader.read()
+    writer.write(my_id)
+    await writer.drain()
+    while True:
+        try:
+            data = await asyncio.wait_for(reader.read(), 60)
+            writer.write(b"ok")
+            start, stop, digest = data.decode().split(",")
+            result = decrypt(start, stop, digest)
+            if result is not None:
+                writer.write(f"success:{result}".encode())
+                await writer.drain()
+                if await asyncio.wait_for(reader.read(), 60) == b"ok":
+                    success = True
+                    break
+            else:
+                writer.write(b"failed")
+                await writer.drain()
+                await reader.read()
+                writer.write(b"next")
+                await writer.drain()
+        except TimeoutError:
+            success = False
+            break
+    if success:
+        writer.write(b"end:success")
+        await writer.drain()
+        await reader.read()
+    else:
+        writer.write(b"end:exit")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
